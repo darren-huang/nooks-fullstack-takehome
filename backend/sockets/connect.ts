04dfb24e7a6d6acbda14c59588b12ff6c9afe83e
Darren Huang <darren.y.huang@gmail.com>
2023-07-19 13:49:28 -0400

    updated disconnection handler

--- backend/sockets/connect.ts
+++ backend/sockets/connect.ts
@@ -1,12 +1,13 @@
-import { Server as SocketServer } from 'socket.io';
+import { Server as SocketServer, Socket } from 'socket.io';
 import sioEvent from 'common';
 
 export default function setupSocket(io: SocketServer): void {
-    io.on(sioEvent.CON, (socket) => {
+    io.on(sioEvent.CON, (socket: Socket) => {
         console.log(`new socket connection: id: ${socket.id}, nsp: ${socket.nsp.name}`);
         socket.emit(sioEvent.CON, null);
-    });
-    io.on(sioEvent.DCON, (socket) => {
-        console.log(`new socket disconnection: id: ${socket.id}, nsp: ${socket.nsp.name}`);
+
+        socket.on(sioEvent.DCON, () => {
+            console.log(`new socket disconnection: id: ${socket.id}, nsp: ${socket.nsp.name}`);
+        });
     });
 }
